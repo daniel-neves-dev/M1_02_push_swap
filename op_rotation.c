058e7d7219ca024e328f100fc813f7e793e62b3e
Gabriel <yuuhekurenai@gmail.com>
2026-07-06 21:53:15 -0300

    Funções - rb e rr construidas.

--- op_rotation.c
+++ op_rotation.c
@@ -20,3 +20,48 @@ void    ra(t_list **stack_a)
 
 	ft_printf("ra\n");
 }
+
+void    rb(t_list **stack_b)
+{
+	t_list  *first;
+	t_list  *last;
+
+	if (!stack_b || !*stack_b || !(*stack_b)->next)
+		return ;
+	first = *stack_b;
+	*stack_b = first->next;
+	first->next = NULL;
+	last = *stack_b;
+	while (last->next)
+		last = last->next;
+	last->next = first;
+	ft_printf("rb\n");
+}
+
+void    rr(t_list **stack_a, t_list **stack_b)
+{
+	t_list  *first;
+	t_list  *last;
+
+	if (stack_a && *stack_a && (*stack_a)->next)
+	{
+		first = *stack_a;
+		*stack_a = first->next;
+		first->next = NULL;
+		last = *stack_a;
+		while (last->next)
+			last = last->next;
+		last->next = first;
+	}
+	if (stack_b && *stack_b && (*stack_b)->next)
+	{
+		first = *stack_b;
+		*stack_b = first->next;
+		first->next = NULL;
+		last = *stack_b;
+		while (last->next)
+			last = last->next;
+		last->next = first;
+	}
+	ft_printf("rr\n");
+}
