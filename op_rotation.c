cbe30fe27953015a87fb278ede091d6944e4da79
Gabriel <yuuhekurenai@gmail.com>
2026-07-07 11:38:55 -0300

    fix - ra não tratava o nulo antes de chegar no while, então causaria seg fault.
    add - adicionado funções restantes para rotation, rrr  e rrb funciona de forma similar à ss, então não chamei via função mas repliquei cada, assim não quebra.

--- op_rotation.c
+++ op_rotation.c
@@ -5,23 +5,19 @@ void    ra(t_list **stack_a)
 	t_list  *first;
 	t_list  *last;
 
-	if (!stack_a || !*stack_a)
-		return;
-
+	if (!stack_a || !*stack_a || !(*stack_a)->next)
+		return ;
 	first = *stack_a;
 	*stack_a = first->next;
-	first->prev = NULL;
-
+	first->next = NULL;
 	last = *stack_a;
-	while (last->next != NULL)
+	while (last->next)
 		last = last->next;
 	last->next = first;
-	first->next = NULL;
-
 	ft_printf("ra\n");
 }
 
-void    rb(t_list **stack_b)
+void	rb(t_list **stack_b)
 {
 	t_list  *first;
 	t_list  *last;
@@ -38,7 +34,7 @@ void    rb(t_list **stack_b)
 	ft_printf("rb\n");
 }
 
-void    rr(t_list **stack_a, t_list **stack_b)
+void	rr(t_list **stack_a, t_list **stack_b)
 {
 	t_list  *first;
 	t_list  *last;
@@ -64,4 +60,58 @@ void    rr(t_list **stack_a, t_list **stack_b)
 		last->next = first;
 	}
 	ft_printf("rr\n");
+}
+
+void	rrb(t_list **stack_b)
+{
+	t_list  *prev;
+	t_list  *last;
+
+	if (!stack_b || !*stack_b || !(*stack_b)->next)
+		return ;
+	prev = NULL;
+	last = *stack_b;
+	while (last->next)
+	{
+		prev = last;
+		last = last->next;
+	}
+	prev->next = NULL;
+	last->next = *stack_b;
+	*stack_b = last;
+	ft_printf("rrb\n");
+}
+
+void	rrr(t_list **stack_a, t_list **stack_b)
+{
+	t_list  *prev;
+	t_list  *last;
+
+	if (stack_a && *stack_a && (*stack_a)->next)
+	{
+		prev = NULL;
+		last = *stack_a;
+		while (last->next)
+		{
+			prev = last;
+			last = last->next;
+		}
+		prev->next = NULL;
+		last->next = *stack_a;
+		*stack_a = last;
+	}
+	if (stack_b && *stack_b && (*stack_b)->next)
+	{
+		prev = NULL;
+		last = *stack_b;
+		while (last->next)
+		{
+			prev = last;
+			last = last->next;
+		}
+		prev->next = NULL;
+		last->next = *stack_b;
+		*stack_b = last;
+	}
+	ft_printf("rrr\n");
 }
